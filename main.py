9cb3e18f19bf700543a8156a476847f409176802
eggss <eggss@COOPAS_LAPTOP>
2026-05-04 18:26:09 -0600

    Null player name case handling

--- main.py
+++ main.py
@@ -16,32 +16,33 @@ class MainWindow(QMainWindow):
         uic.loadUi("PlayerInput.ui", self)
 
 
-        self.addPlayerButton.clicked.connect(self.AddPlayer)
-        self.removePlayerButton.clicked.connect(self.RemovePlayer)
+        self.addPlayerButton.clicked.connect(self.AddPlayer)    #Add player button
+        self.removePlayerButton.clicked.connect(self.RemovePlayer)  #Remove player button
 
     def AddPlayer(self):
         global players
         name = self.playerRegisterBox.toPlainText().strip()
-        playerToRegister = Player(name)
+        if name != "":  #Esnure name cannot be empty
+            playerToRegister = Player(name)
 
-        players.append(playerToRegister)    #Add instance of player to global array
+            players.append(playerToRegister)    #Add instance of player to global array
 
-        self.playerList.addItem(playerToRegister.name)      #Add name to list
-        self.playerRegisterBox.clear()  #Clear input box
+            self.playerList.addItem(playerToRegister.name)      #Add name to list
+            self.playerRegisterBox.clear()  #Clear input box
 
 
     def RemovePlayer(self):
         global players
-        item = self.playerList.currentItem()
+        item = self.playerList.currentItem()    #Get the name of the selected item
         name = item.text()
 
         for player in players:
-            if name == player.name:
+            if name == player.name:     #Find the correct instance of Player to remove using names in list
                 playerToRemove = player
 
-        players.remove(playerToRemove)
+        players.remove(playerToRemove)  #Remove from internal player list
         index = self.playerList.currentRow()
-        self.playerList.takeItem(index)
+        self.playerList.takeItem(index) #Remove from UI list
 
 
 app = QApplication(sys.argv)
